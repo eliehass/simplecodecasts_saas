fbe556f841479abba5ec0adb8cd53bd462465db9
Elie Hassan <eliehass@mac.com>
2016-03-15 17:48:31 +0000

    added mailer and view

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,13 @@
+class COntactMailer < ActionMailer::Base
+    
+    default to: 'eliehass@mac.com'
+
+    def contact_email(name, email, body)
+        @name = name
+        @email = email
+        @body = body
+        
+        mail(from: email, subject: 'Contact Form Message')
+    end
+
+end
